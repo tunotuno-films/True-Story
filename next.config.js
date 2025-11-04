ea2091418cd3303207294826cfc0be6bd4b1449b
Yuuki Tsunoda (角田 悠綺) <tunotunotrain@gmail.com>
2025-11-04 16:56:37 +0900

    youtube bug fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   // output: 'export',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self' https://www.youtube.com https://youtube.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
