32a92e66ed56db54de58497f88403515a22b3bb9
Renshi Suzuki <renshi15sr@gmail.com>
2025-11-04 15:01:13 +0900

    2025/11/04 .next  管理対象から外す

--- .next/dev/static/chunks/src_app_about_page_tsx_c2c86fb2._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_2ac00e8a._.js",
-  "static/chunks/node_modules_f04d3404._.js"
-],
-    source: "dynamic"
-});
