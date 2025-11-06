d50b3dda1f865a0cae175692b010ffdafb45d10e
Yuuki Tsunoda (角田 悠綺) <tunotunotrain@gmail.com>
2025-11-06 23:45:32 +0900

    1106_03_bug fix

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://npxqbgysjxykcykaiutm.supabase.co https://www.google.com https://www.googleadservices.com https://googleads.g.doubleclick.net https://t.co https://analytics.twitter.com https://via.placeholder.com https://i.ytimg.com; frame-src 'self' https://www.youtube.com https://youtube.com; connect-src 'self' https://www.youtube.com https://npxqbgysjxykcykaiutm.supabase.co;",
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://npxqbgysjxykcykaiutm.supabase.co https://www.google.com https://www.googleadservices.com https://googleads.g.doubleclick.net https://t.co https://analytics.twitter.com https://placehold.co https://i.ytimg.com; frame-src 'self' https://www.youtube.com https://youtube.com; connect-src 'self' https://www.youtube.com https://npxqbgysjxykcykaiutm.supabase.co;",
           },
         ],
       },
